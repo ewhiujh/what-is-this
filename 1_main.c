fc80a762f4e2a704c17362478dd4df5cf17738ed
ewhiujh <2145715545@qq.com>
2025-10-20 00:15:51 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,13 @@
-//
-//
-//
+//202510115202
+//2145715545@qq.com
+//黄家凤
 #include <stdio.h>
 int main()
 {
-    printf("Hello World");
+    float a,b;
+    printf("摄氏温度为");
+    scanf("%f",&a);
+    b = a * 9.0 / 5.0 + 32.0;
+    printf("华氏温度为%.1f\n",b) ;
     return 0;
-}
+}//
