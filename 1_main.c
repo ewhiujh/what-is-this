b4602242f9c359e510b58c106da30afaf41675fc
ewhiujh <2145715545@qq.com>
2025-10-27 00:28:09 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -7,14 +7,21 @@ int main()
     int n = 0;
     int a = 0;
     scanf("%d",&n);
+    while(1){
+    if(n < 1 || n >= 50 )
+    {
+        printf("数字无效")；
+            continue;
+    }
     for(a=2;a<=n;a++)
     {
     if(n%a==0)
-    break;
+      break;
     }
-    if(a<n || n>=50)
-    printf("密钥不安全，请重新输入");
+    if(a<n)
+      printf("密钥不安全，请重新输入");
     else
-    printf("密钥安全，密码设置成功");
+      printf("密钥安全，密码设置成功");
+    }
     return 0;
 }
