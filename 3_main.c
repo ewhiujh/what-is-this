b7b63621511c53bbeaba5c6894a1de16dd151442
ewhiujh <2145715545@qq.com>
2025-10-27 00:11:03 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -4,28 +4,21 @@
 #include <stdio.h>
 int main()
 {
-    int a,b;
-    char c;
-    printf("请输入两个整数和一个运算符");
-    scanf("%d %d %c", &a,&b,&c);
-    switch(c){
-        case '+':
-        printf("%d\n",a + b);
-        break;
-        case '-':
-        printf("%d\n",a - b);
-        break;
-        case '*':
-        printf("%d\n",a * b);
-        break;
-        case '/':
-        if (b != 0){
-        printf("%d\n",a / b);
-    }
-    else {
-        printf("除数不为0");
-    }
-        break;
-    }
+    int n = 0;
+    int a = 2;
+    scanf("%d",&n);
+    while(a<n)
+{
+    if(n%a==0)
+   { 
+    break;
+   }
+    else
+    a++;
+}
+    if(a < n || n >= 50)
+    printf("密钥不安全，请重新输入");
+    else
+    printf("密钥安全，密码设置成功");
     return 0;
 }
