a1466c301019980a91e80c6ede7bbd10106633a6
ewhiujh <2145715545@qq.com>
2025-10-27 00:09:32 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -4,10 +4,17 @@
 #include <stdio.h>
 int main()
 {
-    float a,b;
-    printf("摄氏温度为");
-    scanf("%f",&a);
-    b = a * 9.0 / 5.0 + 32.0;
-    printf("华氏温度为%.1f\n",b) ;
+    int n = 0;
+    int a = 0;
+    scanf("%d",&n);
+    for(a=2;a<=n;a++)
+    {
+    if(n%a==0)
+    break;
+    }
+    if(a<n || n>=50)
+    printf("密钥不安全，请重新输入");
+    else
+    printf("密钥安全，密码设置成功");
     return 0;
-}//
+}
