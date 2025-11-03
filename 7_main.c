7bac28f6553deaf758e02cd491793218a3e734a8
ewhiujh <2145715545@qq.com>
2025-11-03 19:19:11 +0800

    Update 7_main.c

--- 7_main.c
+++ 7_main.c
@@ -2,22 +2,24 @@
 //2145715545@qq.com
 //黄家凤
 #include <stdio.h>
-int main()
+void aa(int arr[],int i)
+{
+    for(int a = 0;a<i/2;a++)
+    {
+        int b = arr[a];
+        arr[a]=arr[i-1-a];
+        arr[i-1-a]=b;
+    }
+}
+   int main()
 {
     int arr[5];
-    int a = 0;
-    for(a=0;a<4;a++)
+    for(int a=0;a<5;a++)
     {
         scanf("%d",&arr[a]);
     }
-    a = 4;
-    while(a>0)
-    {
-        arr[a] = arr[a-1];
-        a--;
-    }
-    arr[0] = 0;
-    for(a=0;a<5;a++)
+    aa(arr,5);
+    for(int a = 0;a<5;a++)
     {
         if(a==0)
         {
