8fb967d2a0404f43d4e9f6ed0c82a66783dede10
ewhiujh <2145715545@qq.com>
2025-11-03 19:13:45 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -4,25 +4,14 @@
 #include <stdio.h>
 int main()
 {
-    int n = 0;
-    int a = 0;
-    scanf("%d",&n);
-    while(1){
-    if(n < 1 || n >= 50 )
+    int arr[3][3];
+    for(int a=0;a<3;a++)
     {
-        printf("数字无效");
-        break;
+        scanf("%d %d %d",&arr[a][0],&arr[a][1],&arr[a][2]);
     }
-    for(a=2;a<=n;a++)
+    for(int a=0;a<3;a++)
     {
-    if(n%a==0)
-      break;
-    }
-    if(a<n || n==1)
-      printf("密钥不安全，请重新输入");
-    else
-      printf("密钥安全，密码设置成功");
-      break;
+        printf("%d %d %d\n",arr[a][0],arr[a][1],arr[a][2]);
     }
     return 0;
 }
