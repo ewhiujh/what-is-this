caf889e4afbd0bfeebab78123778147f336507d0
ewhiujh <2145715545@qq.com>
2025-11-03 19:16:59 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -2,32 +2,22 @@
 //2145715545@qq.com
 //黄家凤
 #include <stdio.h>
-int main()
-{
-    int a = 0;
-    int b = 100;
-    int c = 0;
-    int d = 0;
-    int e = 0;
-    while(b<=999)
- {
-        c=b/100;
-        d=b%100/10;
-        e=b%100%10;
-        if(c * c * c + d * d * d + e * e * e == b)
+   int aa(int a, int b)
     {
-      if(a == 0)
+        int c = 1;
+        for(int d = 0;d<b;d++)
         {
-            printf("%d",b);
+            c *= a;
         }
-        
-        else
-        {
-            printf(" %d",b);
-        }
-        a++;         
+        return c;
     }
-        b++;       
- }
-    return 0;
-}
+    int main()
+    {
+        int e = 0;
+        for(int f=1;f<=5;f++)
+        {
+            e += aa(f,2);
+        }
+        printf("%d\n",e);
+        return 0;
+    }
