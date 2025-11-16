b7bfb74f6add21747ba0a41684c59f74c72eefd7
ewhiujh <2145715545@qq.com>
2025-11-16 22:45:39 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,38 +2,15 @@
 //2145715545@qq.com
 //黄家凤
 #include <stdio.h>
-int main()
-{
-    int arr[10];
-    int a = 0;
-    for(a = 0;a<10;a++)
-    {
-        scanf("%d",&arr[a]);
-    }
-    int b = 0;
-    for(a = 0;a<9;a++)
-    {
-        for(b=0;b<9-a;b++)
-        {
-            if(arr[b] > arr[b+1])
-            {
-                int c = 0;
-                c = arr[b];
-                arr[b] = arr[b+1];
-                arr[b+1] = c;
-            }
-        }
-    }
-    for(a = 0;a<10;a++)
-    {
-        if(a==0)
-        {
-            printf("%d",arr[a]);
-        }
-        else
-        {
-            printf(" %d",arr[a]);
-        }
-    }
+void swap(int *a,int *b){
+    int temp = *a;
+    *a =*b;
+    *b = temp;
+}
+int main(){
+    int c,d;
+    scanf("%d %d",&c,&d);
+    swap(&c,&d);
+    printf("%d %d\n",c,d);
     return 0;
 }
