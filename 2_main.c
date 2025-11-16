71af5cb520cc26c3d8441c019d9f485559150a3f
ewhiujh <2145715545@qq.com>
2025-11-16 22:45:06 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -2,24 +2,17 @@
 //2145715545@qq.com
 //黄家凤
 #include <stdio.h>
-int main()
-{
-    int arr[3][3];
-    int arr2[3][3];
-    for(int a = 0;a<3;a++)
-    {
-        scanf("%d %d %d",&arr2[a][0],&arr2[a][1],&arr2[a][2]);
+int main(){
+    int arr[5];
+    for(int a = 0;a<5;a++){
+        scanf("%d",&arr[a]);
     }
-    for(int a=0;a<3;a++)
-    {
-        for(int b = 0;b<3;b++)
-        {
-            arr[b][a]=arr2[a][b];
-        }
+    int *p = arr;
+    for(int a = 0;a<5;a++){
+        *(p+a)*=2;
     }
-     for(int a = 0;a<3;a++)
-    {
-        printf("%d %d %d\n",arr[a][0],arr[a][1],arr[a][2]);
+    for(int a = 0;a<5;a++){
+        printf("%d ",arr[a]);
     }
     return 0;
 }
